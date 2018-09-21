73d4f35cd4678f04ec6d8b65de36f3793a708e46
andrewjohnso7 <andrewjohnso17@gmail.com>
2018-09-21 15:24:42 -0700

    Add solutions to linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -5,21 +5,41 @@ var LinkedList = function() {
 
   list.addToTail = function(value) {
     list[value] = Node(value);
-    if (list.head.next === null && list.head.value === null) {
+    if (list.head.value === null && list.head.next === null) {
       list.head.value = list[value].value;
       list.tail.value = list[value].value;
     } else if (list.head.next === null) {
+      list.tail.value = list[value].value;
       list.head.next = list.tail.value;
-      list.tail.value = list[value].value;
     } else {
-      list.tail.value = list[value].value;
+      list.tail = list[value];
+    }
+  };
+  
+  list.removeHead = function() {
+    if (list.head.value === null) {
+      return null;
+    } else if (list.head.next === null) {
+      var headCurrent = list.head.value;
+      var nullNode = Node(null);
+      list.head = nullNode;
+      list.tail = nullNode;
+      return headCurrent;
+    } else {
+      var headCurrent = list.head.value;
+      delete list[list.head.value];
+      list.head.value = list.head.next;
+      return headCurrent;
     }
   };
 
-  list.removeHead = function() {
-  };
-
   list.contains = function(target) {
+    for (var key in list) {
+      if (list[key].value === target) {
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
@@ -36,8 +56,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addToTail = O()
+ * removeHead = O()
+ * contains = O()
  */
-
-/*
-on the first 
-*/
