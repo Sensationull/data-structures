8cd661eaff4defd6688191bc3c5c889a47f5f5fb
andrewjohnso7 <andrewjohnso17@gmail.com>
2018-09-23 13:32:41 -0700

    Add time complexities for hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -44,6 +44,9 @@ HashTable.prototype.remove = function(k) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * insert = O(1) - constant
+ * retrieve = O(1) - constant
+ * remove = O(1) - constant
  */
 
 
--- sprint-two/src/hashTableHelpers.js
+++ sprint-two/src/hashTableHelpers.js
@@ -56,4 +56,9 @@ var getIndexBelowMaxForKey = function(str, max) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * limitedArray.get = O(1) - constant
+ * limitedArray.set = O(1) - constant
+ * limitedArray.each = O(n) - linear
+ * checkLimit = O(1) - constant
+ * getIndexBelowMaxForKey = O(n) - linear
  */
