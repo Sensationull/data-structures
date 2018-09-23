22fdcda8baa948706dfbf8182fde141faceb82c2
andrewjohnso7 <andrewjohnso17@gmail.com>
2018-09-22 18:02:20 -0700

    Complete set solution

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,34 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this._storage.hasOwnProperty(item)) { 
+    this._storage[item]++;
+  } else {
+    this._storage[item] = 1;
+  }
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage.hasOwnProperty(item)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * add: O(1) - constant
+ * contains: O(1) - constant
+ * remove: O(1) - constant
  */
